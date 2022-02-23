7d7fc897d3f0bbada7687244d65b9c44bedf15b0
tBrause <brause.torsten@gmail.com>
2022-02-24 00:51:30 +0100

    mal sehen

--- inc/einheiten.php
+++
@@ -1,6 +0,0 @@
-<?php
-$einheit_1 = trim(substr(filter_input(INPUT_POST, 'einheit_1'), 0, 6));
-$einheit_2 = trim(substr(filter_input(INPUT_POST, 'einheit_2'), 0, 6));
-$einheit_3 = trim(substr(filter_input(INPUT_POST, 'einheit_3'), 0, 6));
-$einheit_4 = trim(substr(filter_input(INPUT_POST, 'einheit_4'), 0, 6));
-$einheit_5 = trim(substr(filter_input(INPUT_POST, 'einheit_5'), 0, 6));
