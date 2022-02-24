57e60eb1d1663aa376423f3e75df1443e4967fd6
Torsten Brause <89082262+tBrause@users.noreply.github.com>
2022-02-24 01:28:24 +0100

    Update snav.js

--- astrotel_connect/libraries/snav.js
+++ astrotel_connect/libraries/snav.js
@@ -1,2 +1,2 @@
 document.write("<div id='astrotel_nav'><ul><li id='snav_10' style='top:0px;'><span class='icons_extern fa-home'></span><span class='stxt'><a href='tel:030 6489 444'>Telefon</a></span></li></ul></div>");
-// komm scho
+// komm schon
