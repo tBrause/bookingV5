2b9d2dde742d3434c6930888fdf8d8e95bd5a319
tBrause <brause.torsten@gmail.com>
2022-02-24 17:45:25 +0100

    freigabe

--- astrotel_connect/libraries/snavm.js
+++ astrotel_connect/libraries/snavm.js
@@ -1,1 +1,3 @@
-document.write("<div id='astrotel_nav_m'><div id='mclose'><span class='icons_extern fa-close'></span></div><ul><li id='snavm_10'><span class='icons_extern fa-home'></span><span class='stxt'><a href='tel:030 6489 444'>Telefon</a></span></li></ul></div>");
+document.write(
+  "<div id='astrotel_nav'><ul><li id='snav_10' style='top:0px;'><span class='icons_extern fa-home'></span><span class='stxt'><a href='tel:030 6489 444'>Telefon</a></span></li></ul></div>"
+);
