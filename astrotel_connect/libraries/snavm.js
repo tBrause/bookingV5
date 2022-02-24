eda0d504d999e83ad5f6f4ee0e295d7395507cea
Torsten Brause <89082262+tBrause@users.noreply.github.com>
2022-02-24 01:30:00 +0100

    Update snavm.js

--- astrotel_connect/libraries/snavm.js
+++ astrotel_connect/libraries/snavm.js
@@ -1,1 +1,2 @@
 document.write("<div id='astrotel_nav_m'><div id='mclose'><span class='icons_extern fa-close'></span></div><ul><li id='snavm_10'><span class='icons_extern fa-home'></span><span class='stxt'><a href='tel:030 6489 444'>Telefon</a></span></li></ul></div>");
+// jupp
