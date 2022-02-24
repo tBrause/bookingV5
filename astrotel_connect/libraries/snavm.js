655742aad43ba3d144ccb68d3ba0dd9cee75f829
tBrause <brause.torsten@gmail.com>
2022-02-24 17:49:41 +0100

    snavs rm

--- astrotel_connect/libraries/snavm.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-document.write(
-  "<div id='astrotel_nav'><ul><li id='snav_10' style='top:0px;'><span class='icons_extern fa-home'></span><span class='stxt'><a href='tel:030 6489 444'>Telefon</a></span></li></ul></div>"
-);
-=======
-document.write("<div id='astrotel_nav_m'><div id='mclose'><span class='icons_extern fa-close'></span></div><ul><li id='snavm_10'><span class='icons_extern fa-home'></span><span class='stxt'><a href='tel:030 6489 444'>Telefon</a></span></li></ul></div>");
-// jupp
->>>>>>> 7906bdc62408bcef7e62376edbb40c5e5b12bd88
